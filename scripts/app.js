d726f4106c1401d1864e177a205e5d43ad873318
Gromislaf <61653197+Gromislaf@users.noreply.github.com>
2020-07-18 21:04:37 +0200

    koniec końców

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-gdf
+function newElement() 
+{
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById("myUL").appendChild(li);
+  document.getElementById("myInput").value = "";
+}
